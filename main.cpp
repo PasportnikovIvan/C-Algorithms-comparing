145eff00b58e13f5a0a3aca078a7cf0e10307bb7
Kostya22B <kostuyabondarcyhyk2005@gmail.com>
2023-11-28 02:23:19 +0100

    timer which controls how long 2 functions work

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 #include "integral.hpp"
 
 int main() {
@@ -13,17 +14,24 @@ int main() {
         int n = calc.getN();
         std::function<double(double)> function = calc.getFunction();
 
-        ExpressionParser parser;
-        std::function<double(double)> expression = parser.parse("x*x + 2*x + 1");
-
-        double result_left_triangles = calc.leftRectangleIntegration(function, a, b, n);
+        auto start_left_triangles = std::chrono::high_resolution_clock::now();
+        double result_left_triangles = Integral::leftRectangleIntegration(function, a, b, n);
+        auto end_left_triangles = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> duration_left_triangles = end_left_triangles - start_left_triangles;
         std::cout << "Left rectangle integration result: " << result_left_triangles << std::endl;
+        std::cout << "Time taken by left rectangle integration: " << duration_left_triangles.count() << " seconds" << std::endl;
 
-        double result_trapezoidal = calc.trapezoidalIntegration(function, a, b, n);
-        std::cout << "Left rectangle integration result: " << result_trapezoidal << std::endl;
+        auto start_trapezoidal = std::chrono::high_resolution_clock::now();
+        double result_trapezoidal = Integral::trapezoidalIntegration(function, a, b, n);
+        auto end_trapezoidal = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> duration_trapezoidal = end_trapezoidal - start_trapezoidal;
+        std::cout << "Trapezoidal integration result: " << result_trapezoidal << std::endl;
+        std::cout << "Time taken by trapezoidal integration: " << duration_trapezoidal.count() << " seconds" << std::endl;
 
     } else {
         std::cout << "Invalid function parameters" << std::endl;
         return 1;
     }
+
+    return 0;
 }
