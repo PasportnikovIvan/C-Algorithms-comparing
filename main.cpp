b0e7335a062b56531918bd3e2e11200ba2ec591d
Kostya22B <kostuyabondarcyhyk2005@gmail.com>
2023-12-10 03:11:35 +0100

    documentation and submitting the semester work

--- main.cpp
+++ main.cpp
@@ -11,10 +11,10 @@ std::chrono::milliseconds to_ms(TimePoint tp) {
     return std::chrono::duration_cast<std::chrono::milliseconds>(tp);
 }
 void printHelp(){
-    std::cout << "Usage: ./integral {-h | --help } {-s|-a|-p} {-S|-F}" << std::endl;
+    std::cout << "Usage: ./integral {-h | --help } {-lr|-tr|-plr|-ptr} {-e|--exit}" << std::endl;
     std::cout << "Format for inserting the function: function in c++ format(only x variable is supported), down border, up border, precision(count of subintervals)" << std::endl;
     std::cout << "Example1: -lr 2*x,0,2,100" << std::endl;
-    std::cout << "Example2: -tr x^2,-3,2,1000" << std::endl;
+    std::cout << "Example2: -tr x^2+4x-31,-3,2,1000" << std::endl;
     std::cout << "Example3: -plr sin(x),1,5,5" << std::endl;
     std::cout << "Options:" << std::endl;
     std::cout << "  -h, --help Show this help message and exit" << std::endl;
@@ -30,19 +30,17 @@ int main() {
         std::cin >> input;
         if (input == "--exit" || input == "-e") {
             return 0;
-        }
-        else if (input == "-h" || input == "--help") {
+        } else if (input == "-h" || input == "--help") {
             printHelp();
             return 0;
-        }
-        else if (input == "-lr") {
+        } else if (input == "-lr") {
             Integral calc;
             calc.read(std::cin);
             if (calc.isValid()) {
                 auto start_left_triangle = std::chrono::high_resolution_clock::now();
                 calc.printSolution(leftRectangleIntegralSolver::leftRectangleIntegration);
                 auto end_left_triangle = std::chrono::high_resolution_clock::now();
-                std::cout << "Needed " << to_ms(end_left_triangle - start_left_triangle).count() << " ms to finish.\n";
+                std::cout << "Needed time " << to_ms(end_left_triangle - start_left_triangle).count() << " ms\n";
             }
         } else if (input == "-plr") {
             Integral calc;
@@ -51,7 +49,7 @@ int main() {
                 auto start_left_triangle = std::chrono::high_resolution_clock::now();
                 calc.printSolution(ParallelLeftTriangleIntegralSolver::leftRectangleIntegration);
                 auto end_left_triangle = std::chrono::high_resolution_clock::now();
-                std::cout << "Needed " << to_ms(end_left_triangle - start_left_triangle).count() << " ms to finish.\n";
+                std::cout << "Needed time " << to_ms(end_left_triangle - start_left_triangle).count() << " ms\n";
             }
         } else if (input == "-tr") {
             Integral calc;
@@ -60,7 +58,7 @@ int main() {
                 auto start_left_triangle = std::chrono::high_resolution_clock::now();
                 calc.printSolution(trapezoidalIntegralSolver::trapezoidalIntegration);
                 auto end_left_triangle = std::chrono::high_resolution_clock::now();
-                std::cout << "Needed " << to_ms(end_left_triangle - start_left_triangle).count() << " ms to finish.\n";
+                std::cout << "Needed time " << to_ms(end_left_triangle - start_left_triangle).count() << " ms\n";
             }
         } else if (input == "-ptr") {
             Integral calc;
@@ -69,10 +67,9 @@ int main() {
                 auto start_left_triangle = std::chrono::high_resolution_clock::now();
                 calc.printSolution(ParallelTrapezoidalIntegralSolver::trapezoidalIntegration);
                 auto end_left_triangle = std::chrono::high_resolution_clock::now();
-                std::cout << "Needed " << to_ms(end_left_triangle - start_left_triangle).count() << " ms to finish.\n";
+                std::cout << "Needed time " << to_ms(end_left_triangle - start_left_triangle).count() << " ms\n";
             }
+        } else {
+            printHelp();
         }
-        else {
-            std::cout << "Invalid method. Use -h,--help to see supported methods" << std::endl;
-        }
-}
+    }
